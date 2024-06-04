3f68836a5089d83f28a7f4b76d595d70ddb58838
Felix Bade <flix.bade@gmail.com>
2024-06-04 18:45:22 +0300

    add pausing and unpausing again

--- setup.js
+++ setup.js
@@ -2,7 +2,9 @@ function now () {
     return Date.now() / 1000
 }
 
-let startTime = now()
+let startTime = null
+let demoTime = 0
+let paused = true
 
 window.addEventListener('keydown', (event) => {
     // F = toggle fullscreen
@@ -15,10 +17,17 @@ window.addEventListener('keydown', (event) => {
         }
     }
 
-    // Space = start drawing
+    // Space = pause/unpause
     if (event.key === ' ') {
-        loop()
-        startTime = now()
+        if (paused) {
+            paused = false
+            startTime = now() - demoTime
+            loop()
+        } else {
+            paused = true
+            demoTime = now() - startTime
+            noLoop()
+        }
     }
 })
 
